23b8f2eda41e5e3f44e0ab36e3c7c5dfae55167b
某己 <1599725106@qq.com>
2018-12-23 20:09:14 +0800

    v0.0.1

---
+++ bored-app/public/precache-manifest.cd88c689af6eb3e10e84de4da526c08d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39abd60e2fa9a3a26ff6",
+    "url": "/static/js/main.39abd60e.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "39abd60e2fa9a3a26ff6",
+    "url": "/static/css/main.bb77c5cb.chunk.css"
+  },
+  {
+    "revision": "06c5ae2574b4788d9823d03d9169adce",
+    "url": "/index.html"
+  }
+];
